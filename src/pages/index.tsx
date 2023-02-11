66c51dd347bba4080747696582ba4f96901d9518
keitakn <keita.koga.work@gmail.com>
2023-02-12 01:30:33 +0900

    :sparkles: #12 Testing Library を使ったテストを書く為にSample用のComponentを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Grid, Skeleton, Container } from '@mantine/core';
 import type { NextPage } from 'next';
+import { TitleText } from '@/components';
 
 const child = <Skeleton height={140} radius="md" animate={false} />;
 
 const IndexPage: NextPage = () => {
   return (
     <Container my="md">
+      <TitleText title="Next Sample App" />
       <Grid>
         <Grid.Col xs={4}>{child}</Grid.Col>
         <Grid.Col xs={8}>{child}</Grid.Col>
