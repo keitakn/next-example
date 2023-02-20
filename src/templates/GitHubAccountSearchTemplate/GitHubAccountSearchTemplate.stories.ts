aa6d7497a3d190001ead7e51c1f43b0b461dc8e8
keitakn <keita.koga.work@gmail.com>
2023-02-20 13:29:53 +0900

    :white_check_mark: #21 GitHubのAPIからエラーが返ってきた時のStoryを追加

--- src/templates/GitHubAccountSearchTemplate/GitHubAccountSearchTemplate.stories.ts
+++ src/templates/GitHubAccountSearchTemplate/GitHubAccountSearchTemplate.stories.ts
@@ -1,6 +1,10 @@
 import type { ComponentStoryObj } from '@storybook/react';
 import { rest } from 'msw';
-import { mockFetchGitHubAccount } from '@/mocks';
+import {
+  mockFetchGitHubAccount,
+  mockFetchGitHubAccountUnexpectedResponseBody,
+  mockNotFoundError,
+} from '@/mocks';
 import { GitHubAccountSearchTemplate } from '@/templates';
 
 const story = {
@@ -16,6 +20,27 @@ export const Default: Story = {
     msw: {
       handlers: [
         rest.get('https://api.github.com/users/*', mockFetchGitHubAccount),
+      ],
+    },
+  },
+};
+
+export const ShowGitHubAccountNotFoundError: Story = {
+  parameters: {
+    msw: {
+      handlers: [rest.get('https://api.github.com/users/*', mockNotFoundError)],
+    },
+  },
+};
+
+export const ShowUnexpectedResponseBodyError: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        rest.get(
+          'https://api.github.com/users/*',
+          mockFetchGitHubAccountUnexpectedResponseBody
+        ),
       ],
     },
   },
