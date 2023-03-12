5b9153d5ec6c146cfa2912167997fa00b372a600
keitakn <keita.koga.work@gmail.com>
2023-03-12 23:24:05 +0900

    :recycle: #7 ComponentPropsWithoutRefのうち利用するPropsだけ指定したいので分割代入で受け取るように変更

--- src/components/GitHubButton/GitHubButton.tsx
+++ src/components/GitHubButton/GitHubButton.tsx
@@ -2,14 +2,11 @@ import type { FC, ComponentPropsWithoutRef } from 'react';
 import { Button } from '@mantine/core';
 import { FaGithub } from 'react-icons/fa';
 
-export const GitHubButton: FC<ComponentPropsWithoutRef<'button'>> = (props) => {
+export const GitHubButton: FC<ComponentPropsWithoutRef<'button'>> = ({
+  onClick,
+}) => {
   return (
-    <Button
-      color="dark"
-      leftIcon={<FaGithub />}
-      onClick={props.onClick}
-      size="xl"
-    >
+    <Button color="dark" leftIcon={<FaGithub />} onClick={onClick} size="xl">
       Sign in with GitHub
     </Button>
   );
